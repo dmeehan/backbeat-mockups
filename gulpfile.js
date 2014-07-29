34d1a8c454da73f07ab86ecec5723f04924479e4
Douglas Meehan <dmeehan@gmail.com>
2014-07-29 00:24:44 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');  
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+var paths = {
+    scss: './sass/*.scss'
+};
+
+gulp.task('sass', function () {  
+    gulp.src('scss/app.scss')
+        .pipe(sass({
+        	includePaths: ['scss']
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('browser-sync', function() {  
+    browserSync.init(["css/*.css", "js/*.js"], {
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['sass']);
+});
